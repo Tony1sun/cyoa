7dcc7ab0ea271b9872a6b0f0f236b395c77801a6
Tony1sun <jacktony822@gmail.com>
2021-08-17 23:04:01 +0800

    提交

--- story.go
+++ story.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
 var defaultHandlerTmpl = `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -70,33 +76,47 @@ var defaultHandlerTmpl = `<!DOCTYPE html>
 </body>
 </html>`
 
-var tpl *template.Template
+type HandlerOption func(h *handler)
 
-func init() {
-	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
 }
 
-func NewHandler(s Story, t *template.Template) http.Handler {
-	if t == nil {
-		t = tpl
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
 	}
-	return handler{s, t}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
-		err := tpl.Execute(w, chapter)
+		err := h.t.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
